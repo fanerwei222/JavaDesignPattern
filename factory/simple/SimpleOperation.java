6494743486b617d70c6e0239b0065d9fb0eacead
樊伟 <fanerwei222@163.com>
2019-01-18 14:26:31 +0800

    Create SimpleOperation.java

---
+++ factory/simple/SimpleOperation.java
@@ -0,0 +1,11 @@
+package main.designPattern.factory.simple;
+
+/**
+ * 简单工厂运算接口
+ * @author fanwei
+ *
+ */
+public interface SimpleOperation
+{
+    public double getResult(double num1, double num2) throws Exception;
+}
