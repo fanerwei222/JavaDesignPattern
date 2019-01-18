5a69c577e92f036e06081195b143ef7f39be21d5
樊伟 <fanerwei222@163.com>
2019-01-18 14:25:05 +0800

    Create MethodOperation.java

---
+++ factory/method/MethodOperation.java
@@ -0,0 +1,11 @@
+package main.designPattern.factory.method;
+
+/**
+ * 方法工厂运算接口
+ * @author fanwei
+ *
+ */
+public interface MethodOperation
+{
+    public double getResult(double num1, double num2) throws Exception;
+}
