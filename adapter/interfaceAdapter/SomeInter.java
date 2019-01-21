afe53a9ba0dcdcee76feee236cb32dc0be6e104b
樊伟 <fanerwei222@163.com>
2019-01-21 09:39:09 +0800

    Create SomeInter.java

---
+++ adapter/interfaceAdapter/SomeInter.java
@@ -0,0 +1,19 @@
+package main.designPattern.adapter.interfaceAdapter;
+
+/**
+ * 任意的接口
+ * @author fanwei
+ *
+ */
+public interface SomeInter
+{
+    void a();
+
+    void b();
+
+    void c();
+
+    void d();
+
+    void e();
+}
