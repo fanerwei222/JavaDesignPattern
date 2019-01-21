e21e82d84691d0938de5e69e821921bcd9da51e0
樊伟 <fanerwei222@163.com>
2019-01-21 09:38:24 +0800

    Create InterAdapter.java

---
+++ adapter/interfaceAdapter/InterAdapter.java
@@ -0,0 +1,46 @@
+package main.designPattern.adapter.interfaceAdapter;
+
+/**
+ * 接口适配器
+ * @author fanwei
+ *
+ */
+public abstract class InterAdapter implements SomeInter
+{
+
+    @Override
+    public void a()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void b()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void c()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void d()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void e()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
