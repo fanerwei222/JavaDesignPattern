1ef0231963455db22edb97249e508e96acb5d246
樊伟 <fanerwei222@163.com>
2019-01-21 15:59:20 +0800

    Create IStrategy.java

---
+++ strategy/IStrategy.java
@@ -0,0 +1,14 @@
+package main.designPattern.strategy;
+
+/**
+ * 策略接口
+ * @author fanwei
+ *
+ */
+public interface IStrategy
+{
+    /**
+     * 定义的抽象算法方法 来约束具体的算法实现方法
+     */
+    public void algorithmMethod();
+}
