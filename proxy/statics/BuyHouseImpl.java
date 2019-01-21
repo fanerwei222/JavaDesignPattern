82f28f3f75d943ff955c8d62cffc1289bd216b3a
樊伟 <fanerwei222@163.com>
2019-01-21 10:49:24 +0800

    Create BuyHouseImpl.java

---
+++ proxy/statics/BuyHouseImpl.java
@@ -0,0 +1,17 @@
+package main.designPattern.proxy.statics;
+
+/**
+ * 买房子实现类
+ * @author fanwei
+ *
+ */
+public class BuyHouseImpl implements BuyHouse
+{
+
+    @Override
+    public void buyHosue()
+    {
+        System.out.println("我要买房");
+    }
+
+}
