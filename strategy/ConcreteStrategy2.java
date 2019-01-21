fb9ef3a3be378d97328699f3c55b0d5de36c3f67
樊伟 <fanerwei222@163.com>
2019-01-21 15:59:10 +0800

    Create ConcreteStrategy2.java

---
+++ strategy/ConcreteStrategy2.java
@@ -0,0 +1,20 @@
+package main.designPattern.strategy;
+
+/**
+ * 具体的策略实现2
+ * @author fanwei
+ *
+ */
+public class ConcreteStrategy2 implements IStrategy
+{
+
+    /**
+     * 具体的算法实现
+     */
+    @Override
+    public void algorithmMethod()
+    {
+        System.out.println("this is ConcreteStrategy2 method...");
+    }
+
+}
