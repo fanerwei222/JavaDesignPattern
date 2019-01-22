e4cdc8da2926ff568da4a71952937b557cb65221
樊伟 <fanerwei222@163.com>
2019-01-22 13:46:04 +0800

    Create Mediator.java

---
+++ mediator/Mediator.java
@@ -0,0 +1,13 @@
+package main.designPattern.mediator;
+
+/**
+ * 抽象中介者
+ * @author fanwei
+ *
+ */
+public abstract class Mediator
+{
+    //申明一个联络方法
+    public abstract void constact(String message, Person person);
+
+}
