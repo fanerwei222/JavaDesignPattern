2448cd06dd8577e21811d8efb5151e5ea7ee4265
樊伟 <fanerwei222@163.com>
2019-01-22 11:37:26 +0800

    Create RoleStateMemento.java

---
+++ memento/RoleStateMemento.java
@@ -0,0 +1,38 @@
+package main.designPattern.memento;
+
+/**
+ * 存储类
+ * @author fanwei
+ *
+ */
+public class RoleStateMemento
+{
+    private int vit;
+    private int atk;
+
+    public RoleStateMemento(int vit, int atk)
+    {
+        this.vit = vit;
+        this.atk = atk;
+    }
+
+    public int getVit()
+    {
+        return vit;
+    }
+
+    public void setVit(int vit)
+    {
+        this.vit = vit;
+    }
+
+    public int getAtk()
+    {
+        return atk;
+    }
+
+    public void setAtk(int atk)
+    {
+        this.atk = atk;
+    }
+}
